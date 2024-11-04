de015d5937236edeef907ed417ea507d0d796216
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-04 23:54:13 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import '../globals.css';
+import './globals.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-gray-100 text-gray-900">
