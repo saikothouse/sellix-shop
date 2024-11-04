462321cd0d6fccb7aa14888fa59c8e1c10b928bc
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-05 00:07:44 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { fetchProducts } from '../utils/sellixApi';
 import ProductGrid from '../components/ProductGrid';
+import SearchBar from '../components/SearchBar';
 
 export default async function HomePage() {
   const products = await fetchProducts();
