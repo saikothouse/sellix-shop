8004387bf6aaf0095af29cc81a36a8f32d1dc3c7
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-05 00:13:20 +0600

    Create CartContext.tsx

---
+++ context/CartContext.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { createContext, useContext, useState, ReactNode } from 'react';
+
+interface CartItem {
+  id: string;
+  title: string;
+  price: number;
+  quantity: number;
+}
+
+interface CartContextProps {
+  cart: CartItem[];
+  addToCart: (item: CartItem) => void;
+  removeFromCart: (id: string) => void;
+  clearCart: () => void;
+}
+
+const CartContext = createContext<CartContextProps | undefined>(undefined);
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error('useCart must be used within a CartProvider');
+  }
+  return context;
+};
+
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [cart, setCart] = useState<CartItem[]>([]);
+
+  const addToCart = (item: CartItem) => {
+    setCart((prevCart) => {
+      const existingItem = prevCart.find((i) => i.id === item.id);
+      if (existingItem) {
+        return prevCart.map((i) =>
+          i.id === item.id ? { ...i, quantity: i.quantity + item.quantity } : i
+        );
+      } else {
+        return [...prevCart, item];
+      }
+    });
+  };
+
+  const removeFromCart = (id: string) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+  };
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
