8abc8bae61a20eaa965bcb1945abd515a36fb256
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-04 23:55:07 +0600

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-blue-600 text-white py-4 mt-8">
+      <div className="container mx-auto px-4 text-center">
+        &copy; 2024 My E-commerce Shop. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
